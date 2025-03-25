3e27fbaa6a15721f8133819a87011227053f3a8e
Gokul Kannan <132426290+Gokul5001@users.noreply.github.com>
2025-03-25 17:10:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import inquirer from "inquirer";
 import qr from "qr-image";
 import fs, { writeFile } from "fs";
 
+// Bug 1: Unused imported module (writeFile is imported but never used)
+// Bug 2: Missing error handling for qr-image import
 inquirer
   .prompt([
     {
@@ -10,19 +12,29 @@ inquirer
     },
   ])
   .then((answers) => {
-    const url =answers.URL;
+    const url =answers.URL;  // Bug 3: Inconsistent spacing around = operator
     var qr_svg = qr.image(url);
+    // Bug 4: No validation of URL input
+    // Bug 5: Hardcoded output filename could cause conflicts
     qr_svg.pipe(fs.createWriteStream('qr_image.png'));
 
+    // Bug 6: Duplicate file writing (same URL written twice)
     fs.writeFile("URL.txt",url,(err)=>{
       if(err) throw err;
       console.log("The file has been saved!");
     });
+    
+    fs.writeFile("URL.txt",url,(err)=>{
+      if(err) throw err;
+      console.log("The file has been saved again!");  // Bug 7: Redundant logging
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+      // Bug 8: Empty if block (no handling)
     } else {
-      // Something else went wrong
+      // Bug 9: Generic error handling that doesn't log the error
     }
   });
+
+// Bug 10: Unhandled promise rejection (no catch for the entire chain)
